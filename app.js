89ddde74e3d89b7da6eced3264222c6d9c463dbe
Elżbieta Krzeczek <krzeczekelzbieta@gmail.com>
2020-05-18 18:20:13 +0200

    fix to url

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const app = express();
 app.get("/", (req, res) => {
   try {
-    axios.get("https://dog.ceo/api/breed/samoyed/images")
+    axios.get("https://dog.ceo/api/breed/samoyed/images/random")
       .then(response => {
         let html = `<html><body></body><img src='${response.data.message}'/></body></html>`;
         res.send(html);
