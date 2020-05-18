895692fd8f35679ca653734dea0f7994f27dfa49
Elżbieta Krzeczek <krzeczekelzbieta@gmail.com>
2020-05-18 17:50:17 +0200

    remove enter

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const axios = require("axios");
 const app = express();
 app.get("/", (req, res) => {
   try {
-    axios
-      .get("https://dog.ceo/api/breeds/image/random")
+    axios.get("https://dog.ceo/api/breeds/image/random")
       .then(response => {
         let html = `<html><body></body><img src='${response.data.message}/>'</body></html>`;
         res.send(html);
